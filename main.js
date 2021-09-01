38167f10f4dc3a77ce85d490dd3909e87f0c7283
BFGDoesGithub <86089724+BFGDoesGithub@users.noreply.github.com>
2021-08-31 19:16:51 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log("my_project");
+var first_name = "Ranbir";
+var last_name = "Kapoor";
+var a = 10;
+var b = 5;
+var add = a + b ;
+var full_name = first_name.concat(last_name);
+console.log(add);
+console.log(full_name);
+
+
